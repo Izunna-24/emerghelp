857565e7bcf60b763c8962c8ef53e5d7f41f2f5c
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-08-01 17:19:01 +0100

    implemented security and  user registration

--- src/test/java/com/emerghelp/emerghelp/services/UserServiceTest.java
+++ src/test/java/com/emerghelp/emerghelp/services/UserServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.emerghelp.emerghelp.data.constants.Gender.UNDEFINED;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,9 +20,12 @@ public class UserServiceTest {
     @DisplayName("test that user can be registered on the system")
     public void registerTest() {
         RegisterUserRequest request = new RegisterUserRequest();
+        request.setFirstName("Jumoke");
+        request.setLastName("Joseph");
         request.setEmail("ridrijulmi2@gufum.com");
         request.setPassword("password");
-
+        request.setGender(UNDEFINED);
+        request.setPhoneNumber("09078480034");
         RegisterUserResponse response = userService.register(request);
         assertNotNull(response);
         assertTrue(response.getMessage().contains("success"));
