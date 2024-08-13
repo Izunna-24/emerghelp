819ae751e3d26a980d2a2a3c90feb3d4860dd987
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-08-13 09:25:04 +0100

    implemented security and  user registration

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -6,7 +6,7 @@ insert into users(user_id ,is_enabled ,email ,password ) values
    (201,true,'jostuyurzo@gufum.com' ,'password' ),
    (202,true,'lepsopogno@gufum.com', '$2a$10$D4z0FTvjGyJFIgD/g6duU.eR9Iouui6lAG96CN2SXkPniup2mktse');
 
-insert into medical_practitioner(id, email, is_available, photo_url, specialization, license_number) values
+insert into medic(id, email, is_available, photo_url, specialization, license_number) values
     (300, 'ridrijulmi@gufum.com', true, 'photo', 'doctor', '400'),
     (301, 'jostuyurzo@gufum.com', true, 'photo', 'nurse', '401');
 
