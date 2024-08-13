74769a9cb2e0cd0471844f392f1401c4e7a7c5cd
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-08-13 08:56:52 +0100

    commit

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -10,11 +10,4 @@ insert into medical_practitioner(id, email, is_available, photo_url, specializat
     (300, 'ridrijulmi@gufum.com', true, 'photo', 'doctor', '400'),
     (301, 'jostuyurzo@gufum.com', true, 'photo', 'nurse', '401');
 
--- INSERT INTO emergency_request (user_id, description, address_id, request_time, request_status, medic_id, medical_report_id)
--- VALUES
---     (1, 'Severe headache and dizziness', 1, NOW(), 'PENDING', 1, 1),
---     (2, 'High fever and weakness', 2, NOW(), 'IN_PROGRESS', 2, 2),
---     (3, 'Chest pain and shortness of breath', 3, NOW(), 'COMPLETED', 3, 3),
---     (4, 'Persistent cough and sore throat', 4, NOW(), 'PENDING', 4, 4),
---     (5, 'Allergic reaction with swelling', 5, NOW(), 'IN_PROGRESS', 5, 5);
 
