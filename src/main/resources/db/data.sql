e8de2967bbf900981c8bc819ac717284f02d2acd
Donald <ericsonericdon6@gmail.com>
2024-08-13 17:10:39 +0100

    Conflict resolved

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -7,10 +7,6 @@ insert into users(id ,is_enabled ,email ,password ) values
    (201,true,'jostuyurzo@gufum.com' ,'password' ),
    (202,true,'lepsopogno@gufum.com', '$2a$10$D4z0FTvjGyJFIgD/g6duU.eR9Iouui6lAG96CN2SXkPniup2mktse');
 
-<<<<<<< HEAD
-insert into medic(id, email, is_enabled , photo_url, specialization, license_number) values
-=======
 insert into medic(id, email, is_enabled, photo_url, specialization, license_number) values
->>>>>>> b0e7cfe53c6b8660b95472707edddb2e37e4512f
     (300, 'ridrijulmi@gufum.com', true, 'photo', 'doctor', '400'),
     (301, 'jostuyurzo@gufum.com', true, 'photo', 'nurse', '401');
--- src/test/java/com/emerghelp/emerghelp/services/impls/MedicServiceTest.java
+++ src/test/java/com/emerghelp/emerghelp/services/impls/MedicServiceTest.java
@@ -3,12 +3,8 @@ package com.emerghelp.emerghelp.services.impls;
 import com.emerghelp.emerghelp.dtos.requests.RegisterMedicRequest;
 import com.emerghelp.emerghelp.dtos.responses.RegisterMedicResponse;
 import com.emerghelp.emerghelp.exceptions.EmailAlreadyExistException;
-<<<<<<< HEAD:src/test/java/com/emerghelp/emerghelp/services/impls/EmergHelpMedicalServiceTest.java
 import com.emerghelp.emerghelp.exceptions.LicenseNumberAlreadyExistException;
-import com.emerghelp.emerghelp.services.MedicalService;
-=======
 import com.emerghelp.emerghelp.services.MedicService;
->>>>>>> b0e7cfe53c6b8660b95472707edddb2e37e4512f:src/test/java/com/emerghelp/emerghelp/services/impls/MedicServiceTest.java
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Sql(scripts = {"/db/data.sql"})
-class MedicServiceTest {
+public class MedicServiceTest {
 
     @Autowired
     private MedicService medicService;
@@ -30,12 +26,7 @@ class MedicServiceTest {
         request.setEmail("eircsonericdon66@gmail.com");
         request.setLicenseNumber("123");
         request.setPassword("password");
-<<<<<<< HEAD:src/test/java/com/emerghelp/emerghelp/services/impls/EmergHelpMedicalServiceTest.java
-        RegisterMedicResponse response = medicalService.register(request);
-=======
-        request.setId(300L);
         RegisterMedicResponse response = medicService.register(request);
->>>>>>> b0e7cfe53c6b8660b95472707edddb2e37e4512f:src/test/java/com/emerghelp/emerghelp/services/impls/MedicServiceTest.java
         assertTrue(response.getMessage().contains("Email sent successfully"));
         assertTrue(true, "Email sent successfully");
         assertEquals("Your account has been created successfully", response.getMessage());
@@ -48,12 +39,7 @@ class MedicServiceTest {
             request.setFirstName("Ikechukwu");
             request.setLicenseNumber("123");
             request.setPassword("password");
-<<<<<<< HEAD:src/test/java/com/emerghelp/emerghelp/services/impls/EmergHelpMedicalServiceTest.java
-            RegisterMedicResponse response1 = medicalService.register(request);
-=======
-            request.setId(300L);
             RegisterMedicResponse response1 = medicService.register(request);
->>>>>>> b0e7cfe53c6b8660b95472707edddb2e37e4512f:src/test/java/com/emerghelp/emerghelp/services/impls/MedicServiceTest.java
             assertTrue(true, "Email sent successfully");
             assertEquals("Your account has been created successfully", response1.getMessage());
         } catch (EmailAlreadyExistException | LicenseNumberAlreadyExistException  exception) {
