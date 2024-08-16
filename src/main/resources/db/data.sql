61d1875cb0c4b64625c291321931dd412a568222
Izuchukwu Ijeudo <151368567+Izunna-24@users.noreply.github.com>
2024-08-16 09:45:06 +0100

    refactored scripts

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,14 +2,14 @@ truncate table users cascade;
 truncate table medic cascade;
 truncate table admin cascade;
 
-insert into users(id ,is_enabled ,email ,password,longitude,latitude ) values
-   (200,true,'ridrijulmi@gufum.com' ,'password' ,3.377417,5.377417),
-   (201,true,'jostuyurzo@gufum.com' ,'password',3.377417,5.377417 ),
-   (202,true,'lepsopogno@gufum.com', '$2a$10$D4z0FTvjGyJFIgD/g6duU.eR9Iouui6lAG96CN2SXkPniup2mktse',3.377417,5.377417);
+insert into users(id ,is_enabled ,email ,password,longitude,latitude, is_logged_in) values
+   (200,true,'ridrijulmi@gufum.com' ,'password' ,3.377417,5.377417, false),
+   (201,true,'jostuyurzo@gufum.com' ,'password',3.377417,5.377417, false),
+   (202,true,'lepsopogno@gufum.com', '$2a$10$D4z0FTvjGyJFIgD/g6duU.eR9Iouui6lAG96CN2SXkPniup2mktse',3.377417,5.377417, false);
 
-insert into medic(id, email, is_enabled, photo_url, specialization, license_number, longitude, latitude) values
-    (300, 'ridrijulmi@gufum.com', true, 'photo', 'doctor', '400', 3.377417, 5.377417),
-    (301, 'jostuyurzo@gufum.com', true, 'photo', 'nurse', '401', 3.377417, 5.377417);
+insert into medic(id, email, photo_url, specialization, license_number, longitude, latitude) values
+    (300, 'ridrijulmi@gufum.com', 'photo', 'doctor', '400', 3.377417, 5.377417),
+    (301, 'jostuyurzo@gufum.com', 'photo', 'nurse', '401', 3.377417, 5.377417);
 
 
 INSERT INTO admin(id, email, password,user_name) values
